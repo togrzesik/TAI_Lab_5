2d8c6696907683db74afa3e12222653b8c49f494
togrzesik <tomasz_grzesik.98@outlook.com>
2020-04-20 11:51:22 +0200

    lab7

--- blog/src/app/shared/search-bar/search-bar.component.ts
+++ blog/src/app/shared/search-bar/search-bar.component.ts
@@ -4,7 +4,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 @Component({
   selector: 'search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
   filterText: string;
